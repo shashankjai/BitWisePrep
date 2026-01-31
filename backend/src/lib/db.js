6fcba06a43b6262b3b5dcf09f944151a9a12a6a1
shashankjai <157102157+shashankjai@users.noreply.github.com>
2026-01-31 15:49:09 +0530

    Enhance connectDB function with error handling
    Added error handling for undefined DB_URL and enabled autoIndex.

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,12 +1,19 @@
-import mongoose from 'mongoose';
-import {ENV} from "./env.js";
+import mongoose from "mongoose";
+import { ENV } from "./env.js";
 
-export const connectDB=async()=>{
- try{
-    const conn=await mongoose.connect(ENV.DB_URL)
+export const connectDB = async () => {
+  try {
+    if (!ENV.DB_URL) {
+      throw new Error("DB_URL is not defined");
+    }
+
+    const conn = await mongoose.connect(ENV.DB_URL, {
+      autoIndex: true,
+    });
+
     console.log(`✅ MongoDB connected : ${conn.connection.host}`);
- } catch(err){
-    console.error(`❌ Error connecting to mongodb : ${err.message}`);
+  } catch (err) {
+    console.error(`❌ Error connecting to MongoDB : ${err.message}`);
     process.exit(1);
- }
-}
+  }
+};
